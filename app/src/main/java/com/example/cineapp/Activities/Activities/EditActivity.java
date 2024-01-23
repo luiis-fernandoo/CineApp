7383feaf47e7d85d538b8b53e6a0c5399bd9b1e3
John <fera55@live.com>
2024-01-23 05:00:32 -0300

    john-feat: user authentication and registration

---
+++ app/src/main/java/com/example/cineapp/Activities/Activities/EditActivity.java
@@ -0,0 +1,76 @@
+package com.example.cineapp.Activities.Activities;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.cineapp.Activities.DAO.UserDao;
+import com.example.cineapp.Activities.Models.User;
+import com.example.cineapp.R;
+
+public class EditActivity extends AppCompatActivity {
+
+    private EditText edit_email, edit_senha, edit_nome, edit_CPF;
+
+    private UserDao userDAO;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit);
+
+        edit_email = findViewById(R.id.edit_email);
+        edit_senha = findViewById(R.id.edit_senha);
+        edit_nome = findViewById(R.id.edit_nome);
+        edit_CPF = findViewById(R.id.edit_CPF);
+        Button bt_salvar = findViewById(R.id.bt_salvar);
+
+        SharedPreferences sp = getSharedPreferences("app_Cassino", Context.MODE_PRIVATE);
+        String savedNome = sp.getString("nome", "");
+        String savedCPF = sp.getString("cpf", "");
+        String savedEmail = sp.getString("email", "");
+        String savedSenha = sp.getString("senha", "");
+
+        edit_nome.setText(savedNome);
+        edit_CPF.setText(savedCPF);
+        edit_email.setText(savedEmail);
+        edit_senha.setText(savedSenha);
+
+        bt_salvar.setOnClickListener(v -> {
+            String nome = edit_nome.getText().toString();
+            String cpf = edit_CPF.getText().toString();
+            String email = edit_email.getText().toString();
+            String senha = edit_senha.getText().toString();
+
+            if (nome.isEmpty() || cpf.isEmpty() || email.isEmpty() || senha.isEmpty()) {
+                Toast.makeText(EditActivity.this, "Preencha todos os campos", Toast.LENGTH_SHORT).show();
+            } else {
+                if (!email.equals(savedEmail)) {
+                    Toast.makeText(EditActivity.this, "Não é possível alterar o e-mail", Toast.LENGTH_SHORT).show();
+                    return; // Impede a execução adicional do código
+                }
+
+                // Atualize o usuário com os novos dados
+                User user = new User(email, nome, senha, cpf);
+                userDAO = new UserDao(getApplicationContext(), user); // Pode ser necessário reinicializar o UserDAO
+                if (userDAO.UserUpdate(user)) {
+                    SharedPreferences.Editor editor = sp.edit();
+                    editor.putString("nome", nome);
+                    editor.putString("cpf", cpf);
+                    editor.putString("senha", senha);
+                    editor.apply();
+
+                    Toast.makeText(EditActivity.this, "Usuário atualizado com sucesso!", Toast.LENGTH_SHORT).show();
+                    finish();
+                } else {
+                    Toast.makeText(EditActivity.this, "Falha ao atualizar o usuário", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+}
--- app/src/main/java/com/example/cineapp/Activities/Helpers/FeedEntryUser.java
+++ app/src/main/java/com/example/cineapp/Activities/Helpers/FeedEntryUser.java
@@ -1,47 +1,41 @@
 package com.example.cineapp.Activities.Helpers;
 
 import android.content.Context;
-import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.provider.BaseColumns;
 import androidx.annotation.Nullable;
 
-import java.util.Date;
-
 public class FeedEntryUser implements BaseColumns {
 
-        public static final String TABLE_NAME = "user";
-        public static final String COLUMN_NAME_NAME = "name";
-        public static final String COLUMN_NAME_EMAIL = "email";
-        public static final String COLUMN_NAME_BIRTH_DATE = "birth_date";
-        public static final String COLUMN_NAME_USERNAME = "username";
+    public static final String TABLE_NAME = "user";
+    public static final String COLUMN_NAME_NAME = "nome";
+    public static final String COLUMN_NAME_EMAIL = "email";
+    public static final String COLUMN_NAME_CPF = "cpf";
+    public static final String COLUMN_NAME_PASSWORD = "senha";
 
-        private static final String SQL_CREATE_ENTRIES =
-                "CREATE TABLE " + com.example.cineapp.Activities.Helpers.FeedEntryFilm.TABLE_NAME + " (" +
-                        FeedEntryUser._ID + " INTEGER PRIMARY KEY, " +
-                        FeedEntryUser.COLUMN_NAME_NAME + " TEXT," +
-                        FeedEntryUser.COLUMN_NAME_EMAIL + "TEXT, " +
-                        FeedEntryUser.COLUMN_NAME_USERNAME + "TEXT, " +
-                        FeedEntryUser.COLUMN_NAME_BIRTH_DATE + "INTEGER )";
+    private static final String SQL_CREATE_ENTRIES =
+            "CREATE TABLE " + FeedEntryUser.TABLE_NAME + " (" +
+                    FeedEntryUser._ID + " INTEGER PRIMARY KEY, " +
+                    FeedEntryUser.COLUMN_NAME_NAME + " TEXT, " +
+                    FeedEntryUser.COLUMN_NAME_EMAIL + " TEXT, " +
+                    FeedEntryUser.COLUMN_NAME_CPF + " TEXT, " +
+                    FeedEntryUser.COLUMN_NAME_PASSWORD + " TEXT)";
 
-        public static class DBHelpers extends SQLiteOpenHelper {
+    public static class DBHelpers extends SQLiteOpenHelper {
 
-
-            public DBHelpers(@Nullable Context context) {
-                super(context, "CineApp.db", null, 1);
-            }
-
-
-            @Override
-            public void onCreate(SQLiteDatabase db) {
-                db.execSQL(SQL_CREATE_ENTRIES);
-            }
-            @Override
-
-            public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
-
-            }
-
+        public DBHelpers(@Nullable Context context) {
+            super(context, "CineApp.db", null, 1);
         }
+
+        @Override
+        public void onCreate(SQLiteDatabase db) {
+            db.execSQL(SQL_CREATE_ENTRIES);
+        }
+
+        @Override
+        public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+            // Implemente a lógica de atualização aqui, se necessário
+        }
+    }
 }
