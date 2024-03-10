1a8a2dcce713fd47b4a250f45394b27416516bbc
Luis <fernandodroidbr6@gmail.com>
2024-03-10 05:55:51 -0300

    Firebase sincronizando

--- app/src/main/java/com/example/cineapp/Activities/Fragments/NotificationFragment.java
+++ app/src/main/java/com/example/cineapp/Activities/Fragments/NotificationFragment.java
@@ -84,12 +84,10 @@ public class NotificationFragment extends Fragment {
         View rootView = inflater.inflate(R.layout.fragment_notification, container, false);
         SharedPreferences sp = getActivity().getSharedPreferences("app", Context.MODE_PRIVATE);
         String savedEmail = sp.getString("email", "");
-        User user = new User();
-        UserDao userDao = new UserDao(getContext(), user);
+        UserDao userDao = new UserDao(getContext(), new User());
         User userID = userDao.getUserNameID(savedEmail);
         List<Reminder> reminders = new ArrayList<>();
-        Reminder reminder = new Reminder();
-        LembreteDao lembreteDao = new LembreteDao(getContext(), reminder);
+        LembreteDao lembreteDao = new LembreteDao(getContext(), new Reminder());
         reminders = lembreteDao.getAllLembretes(userID);
         recyclerView = rootView.findViewById(R.id.recycler_view_reminders);
         recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
